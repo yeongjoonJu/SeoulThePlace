27f4cf724499e6a490c4b768be0664b1bb781b4c
yeongjoon <go1217jo@naver.com>
2018-09-06 08:15:08 +0900

    feat : MainActivity frame

--- app/src/main/java/com/ensharp/seoul/seoultheplace/MainActivity.java
+++ app/src/main/java/com/ensharp/seoul/seoultheplace/MainActivity.java
@@ -1,7 +1,15 @@
 package com.ensharp.seoul.seoultheplace;
 
+import android.annotation.TargetApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.ensharp.seoul.seoultheplace.UIElement.HorizontalListView;
+import com.ensharp.seoul.seoultheplace.UIElement.TagAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> tags = new ArrayList<String>(
+                Arrays.asList(new String[]{"#오늘 서울은?", "#감성적", "#인생샷 서울"}));
+
+        TagAdapter tagAdapter = new TagAdapter(this, tags);
+
+        HorizontalListView tageListView = (HorizontalListView) findViewById(R.id.tagListView);
+        tageListView.setAdapter(tagAdapter);
     }
 }
---
+++ app/src/main/java/com/ensharp/seoul/seoultheplace/UIElement/TagAdapter.java
@@ -0,0 +1,53 @@
+package com.ensharp.seoul.seoultheplace.UIElement;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.ensharp.seoul.seoultheplace.R;
+
+import java.util.ArrayList;
+
+public class TagAdapter extends ArrayAdapter<String> {
+
+    public TagAdapter(Activity context, ArrayList<String> tags) {
+        super(context, 0, tags);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.tagbutton, parent, false);
+        }
+
+        String currentTag = getItem(position);
+
+        final Button tagButton = (Button) listItemView.findViewById(R.id.tagButton);
+        tagButton.setBackground(getContext().getResources().getDrawable(R.drawable.item_unchoicedtag));
+        tagButton.setText(currentTag);
+        tagButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tagButton.setBackground(getContext().getResources().getDrawable(R.drawable.item_choicedtag));
+                tagButton.setTextColor(Color.WHITE);
+            }
+        });
+
+        return listItemView;
+    }
+}
